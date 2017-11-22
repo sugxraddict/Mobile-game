871bd8278c0a840666b5a821d399a8004664b2dd
wakamantroy <wakamantroy@users.noreply.github.com>
2017-11-22 15:59:36 +0100

    Add files via upload

---
+++ Camera/CameraMotor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+    public GameObject _player;
+    public Transform cam;
+
+    public float transition = 0.0f;
+    public float animationDuration = 4.0f;
+    public Vector3 animationOffset = new Vector3(0, 5, 5);
+    private bool animPlay;
+
+    // Use this for initialization
+    void Start()
+    {
+        lookAt = _player.transform;
+        startOffset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animation();
+    }
+
+    public void Animation()
+    {
+        moveVector = lookAt.position + startOffset;
+
+        // X
+        moveVector.x = 0;
+
+        // Y
+        moveVector.y = 5; //Mathf.Clamp(moveVector.y, 3, 5);
+
+        // Z
+        //moveVector.z = 1;
+
+        if (transition > 1.0f)
+        {
+            transform.position = moveVector;
+            animPlay = true;
+        }
+        else
+        {
+            // Animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+            transform.LookAt(lookAt.position + Vector3.up);
+
+        }
+
+    }
+}
---
+++ Player/PlayerMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+    public Transform _player;
+
+    public float speed = 5.0f;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+
+    private float animationDuration = 4.0f;
+
+    private bool isDead = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(_player.position.x);
+
+        Border(_player.position.x);
+
+        if (isDead)
+        {
+            return;
+        }
+
+        if(Time.time < animationDuration)
+        {
+            controller.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+        moveVector = Vector3.zero;
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -0.5f;
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        // X - Left and Right
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        // Y - Up and Down
+
+        // Z - Forward and Backward
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+    //It is being called ebery time our capsule hits something
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.point.z > transform.position.z + controller.radius)
+        {
+            Death();
+        }
+    }
+
+    public void Border(float x)
+    {
+
+
+        if (x > 4.5 || x < -5)
+        {
+            Debug.Log("Blocked");
+        }
+    }
+
+    private void Death()
+    {
+        isDead = true;
+        Debug.Log("Death");
+    }
+}
